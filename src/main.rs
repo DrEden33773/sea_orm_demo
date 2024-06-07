eefc53d12142a797d0dc4ea2eaccfe9d50319085
Eden Wang <edwardwang33773@gmail.com>
2024-06-08 05:52:23 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,20 @@ async fn index() -> &'static str {
 }
 
 #[get("/bakeries")]
-async fn bakeries(db: &State<DatabaseConnection>) -> Json<Vec<String>> {
+async fn bakeries(db: &State<DatabaseConnection>) -> Result<Json<Vec<String>>, ErrorResponder> {
   let db = db as &DatabaseConnection;
 
   let bakery_names = find_all_bakeries(db)
     .await
-    .unwrap()
+    .map_err(Into::<ErrorResponder>::into)?
     .into_iter()
     .map(|b| b.name)
-    .collect::<Vec<_>>();
+    .collect::<Vec<String>>();
 
-  Json(bakery_names)
+  Ok(Json(bakery_names))
 }
 
-#[launch] // The "main" function of the program
+#[launch]
 async fn rocket() -> _ {
   let db = match establish_connection().await {
     Ok(db) => db,
@@ -31,4 +31,26 @@ async fn rocket() -> _ {
   rocket::build()
     .manage(db)
     .mount("/", routes![index, bakeries])
+}
+#[derive(Responder)]
+#[response(status = 500, content_type = "json")]
+struct ErrorResponder {
+  message: String,
+}
+impl From<DbErr> for ErrorResponder {
+  fn from(err: DbErr) -> ErrorResponder {
+    ErrorResponder {
+      message: err.to_string(),
+    }
+  }
+}
+impl From<String> for ErrorResponder {
+  fn from(string: String) -> ErrorResponder {
+    ErrorResponder { message: string }
+  }
+}
+impl From<&str> for ErrorResponder {
+  fn from(str: &str) -> ErrorResponder {
+    str.to_owned().into()
+  }
 }
